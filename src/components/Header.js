4d2c7ba9ab2fcb867a350ce128434fa30476c47e
ganariya <ganariya2525@gmail.com>
2021-06-20 21:04:17 +0900

    :sparkles: READMEといくつかバグ修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
       <Helmet>
         <title>ganariya</title>
         <meta name="description" content="ganariya's portfolio" />
+        <meta name="robots" content="all" />
         <html lang="ja" />
       </Helmet>
       <header className="text-gray-600 font-bold">
@@ -15,13 +16,13 @@ function Header() {
             <span className="ml-1 text-xl">ganariya</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a href="#story" className="mr-5 hover:text-gray-900">
+            <a href="#story" className="mr-8 hover:text-gray-900">
               Story
             </a>
-            <a href="#character" className="mr-5 hover:text-gray-900">
+            <a href="#character" className="mr-8 hover:text-gray-900">
               Character
             </a>
-            <a href="#works" className="mr-5 hover:text-gray-900">
+            <a href="#works" className="mr-8 hover:text-gray-900">
               Works
             </a>
             <a href="#links" className="mr-5 hover:text-gray-900">
