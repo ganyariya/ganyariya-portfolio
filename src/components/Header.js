0df5495a8a1e032214337df3a4d8cc218069747f
ganariya <ganariya2525@gmail.com>
2021-06-20 13:16:45 +0900

    :bug: react は className

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,25 @@ import React from "react"
 
 function Header() {
   return (
-    <header class="text-gray-500 body-font">
-      <div class="container mx-auto flex flex-wrap px-0 py-6 flex-col md:flex-row items-center">
+    <header className="text-gray-500 body-font">
+      <div className="container mx-auto flex flex-wrap px-0 py-6 flex-col md:flex-row items-center">
         <a
           href="/"
-          class="flex font-medium items-center text-gray-900 mb-4 md:mb-2"
+          className="flex font-medium items-center text-gray-900 mb-4 md:mb-2"
         >
-          <span class="ml-1 text-xl">ganariya</span>
+          <span className="ml-1 text-xl">ganariya</span>
         </a>
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a href="#story" class="mr-5 hover:text-gray-900">
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a href="#story" className="mr-5 hover:text-gray-900">
             Story
           </a>
-          <a href="#character" class="mr-5 hover:text-gray-900">
+          <a href="#character" className="mr-5 hover:text-gray-900">
             Character
           </a>
-          <a href="#works" class="mr-5 hover:text-gray-900">
+          <a href="#works" className="mr-5 hover:text-gray-900">
             Works
           </a>
-          <a href="#links" class="mr-5 hover:text-gray-900">
+          <a href="#links" className="mr-5 hover:text-gray-900">
             Links
           </a>
         </nav>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,14 +2,14 @@ import React from "react"
 
 function Hero() {
   return (
-    <div class="text-gray-700">
-      <div class="container mx-auto mt-40 py-30">
-        <div class="ml-10 text-center md:text-left">
-          <h1 class="text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
-            <span class="block xl:inline">コンテンツに恩返しを。</span>
+    <div className="text-gray-700">
+      <div className="container mx-auto mt-40 py-30">
+        <div className="ml-10 text-center md:text-left">
+          <h1 className="text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
+            <span className="block xl:inline">コンテンツに恩返しを。</span>
             <span
               contentEditable="true"
-              class="block xl:inline mt-4 text-2xl text-gray-600"
+              className="block xl:inline mt-4 text-2xl text-gray-600"
               suppressContentEditableWarning={true}
             >
               ここにテキストを入力...
