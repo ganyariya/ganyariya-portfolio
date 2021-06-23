44c536d14d26078971797330eb777727ff7a72c7
ganariya <ganariya2525@gmail.com>
2021-06-23 20:21:00 +0900

    :sparkles: Typingを追加する

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,37 +1,31 @@
 import React from "react"
+import Typing from "react-typing-animation"
 
 function Hero() {
   return (
     <div className="container">
       <div className="h-screen flex justify-center flex-col items-center md:items-start">
         <h1 className="text-3xl tracking-tight font-bold text-gray-900 md:text-4xl md:ml-20">
-          <span className="block ">コンテンツに恩返しを。</span>
-          <span
-            contentEditable="true"
+          <Typing speed={80} className="block ">
+            猫と和解せよ。
+            <Typing.Delay ms={11000} />
+            <Typing.Backspace count={10} />
+            猫デ和解セヨ。
+          </Typing>
+          <Typing
+            speed={80}
             className="block  mt-4 text-2xl text-gray-700 mb-60"
-            suppressContentEditableWarning={true}
           >
-            ここに人生を入力...
-          </span>
+            <Typing.Delay ms={2500} />
+            ここにテキストを入力...
+            <Typing.Delay ms={1500} />
+            <Typing.Backspace count={10} />
+            <Typing.Delay ms={2000} />
+            人生を入力。
+          </Typing>
         </h1>
       </div>
     </div>
-    // <div className="text-gray-700 my-96">
-    //   <div className="container mx-auto py-30 w-10/12">
-    //     <div className=" text-center md:text-left">
-    //       <h1 className="text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
-    //         <span className="block ">コンテンツに恩返しを。</span>
-    //         <span
-    //           contentEditable="true"
-    //           className="block  mt-4 text-2xl text-gray-700"
-    //           suppressContentEditableWarning={true}
-    //         >
-    //           ここに人生を入力...
-    //         </span>
-    //       </h1>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
