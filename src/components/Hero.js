0d45b77dd8dcbe47877ebe3c42e72fa6b67e6dcc
ganariya <ganariya2525@gmail.com>
2021-06-23 22:12:27 +0900

    :sparkles: black

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,37 @@
 import React from "react"
 import Typing from "react-typing-animation"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef, useState } from "react"
+
+function Box(props) {
+  const mesh = useRef()
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += 0.04
+    mesh.current.rotation.y += 0.02
+  })
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 0.4 : 0.2}
+      onClick={event => setActive(!active)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="black" />
+    </mesh>
+  )
+}
 
 function Hero() {
   return (
-    <div className="container">
+    <div className="container relative">
+      <div className="h-screen w-full absolute">
+        <Canvas>
+          <Box position={[0, -2, 0]} />
+        </Canvas>
+      </div>
+
       <div className="h-screen flex justify-center flex-col items-center md:items-start">
         <h1 className="text-3xl tracking-tight font-bold text-gray-900 md:text-4xl md:ml-20">
           <Typing speed={80} className="block ">
