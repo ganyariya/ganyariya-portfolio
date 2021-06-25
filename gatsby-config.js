fedb1e972cee9694670feda24c0c9ac04cc8506c
ganariya <ganariya2525@gmail.com>
2021-06-25 18:49:24 +0900

    :sparkles: add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,14 +25,19 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-        icon: `src/images/ganyariya_transparent.png`, // This path is relative to the root of the site.
+        icon: `src/images/ganyariya_transparent.png`,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-XW2X916G1Q"],
+        pluginConfig: {
+          head: false,
+        },
+      },
+    },
   ],
 }
