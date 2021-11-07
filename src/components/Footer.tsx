484d4fc52778b2c6fc40f4cf10ce06a808e2a44a
ganariya <ganariya2525@gmail.com>
2021-11-07 11:42:01 +0900

    :sparkles: add counter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,14 @@ const Footer = () => {
     <div className="container mx-auto px-6 mt-40">
       <div className="mt-32 border-t-2 border-gray-700 flex flex-col items-center">
         <div className="sm:w-2/3 text-center py-6">
-          <p className="text-sm text-gray-800 font-bold mb-2">
+          <img
+            className="mx-auto h-12 mb-4"
+            src="https://count.getloli.com/get/@ganyariya-portfolio?theme=rule34"
+            alt="ganyariya-portfolio"
+          />
+          <div className="text-sm text-gray-800 font-bold mb-2">
             © 2021 by ganyariya
-          </p>
+          </div>
         </div>
       </div>
     </div>
